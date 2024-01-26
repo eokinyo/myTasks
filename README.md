6c62a15bd3e8a940bb12503ba78a8bec2fdd65ac
eokinyo <50834185+eokinyo@users.noreply.github.com>
2024-01-26 10:58:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# src
+ School project - Web Programming
---
+++ hello.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Test</title>
+</head>
+<body>
+    <!--HTML comments are differnt from PHP comments-->
+<?php 
+//This is printing "Hello World"
+#we can also write comments like this.
+echo "<h1>Hello World</h1>";
+/*We can also 
+have multi-line 
+comments*/
+// In php, key words are not case sensitive. However, variable names are case sesnsitive.
+?>   
+</body>
+</html>
+
+
