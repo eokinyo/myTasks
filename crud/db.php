242918428ff22647225774d1928e6feb7d884cac
eokinyo <50834185+eokinyo@users.noreply.github.com>
2024-02-05 10:58:26 +0200

    crud

---
+++ crud/db.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "web-dev-env-main-db-1";
+$username = "app1";
+$password = "@!noSalmi1993"
+$dbname = "App1"
+
+//Create a database connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+
+if($conn->connect_error){
+    die("Connection Failed: " . $conn->connect_error);
+}
+?>
---
+++ crud/process.php
@@ -0,0 +1,29 @@
+<?php
+// What to do with the data.
+if (isset($_POST['submit'])){
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $city = $_POST['city'];
+    $groupid = $_POST['groupid'];
+
+// The data is now saved. We now connect to the database server
+//Connect to the database server
+
+include 'db.php';
+
+//Next, write sql statements to insert the data
+
+$sql = "insert into studentsinfo(first_name, last_name, city, groupId)
+        values('$fname', '$lname', '$city', '$groupid')";
+
+if ($conn->($sql)===TRUE){
+    echo "Your data was recorded";
+}
+else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+//Closs the 
+$conn->close;
+}
+?>
